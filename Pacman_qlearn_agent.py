f7be478ab234c1f736e4581a74e2de3a4556c71b
Akshat69 <59080228+Akshat69@users.noreply.github.com>
2020-05-14 23:27:08 +0530

    Create Pacman_qlearn_agent.py

---
+++ Pacman_qlearn_agent.py
@@ -0,0 +1,47 @@
+import gym
+import math
+import numpy as np
+
+env = gym.make('MsPacman-v0')
+
+episodes = 25000
+show_every = 1000
+learn_rate = 0.01
+dicount = 0.95
+epsilon = 1
+epsilon_start_decay = 1
+epsilon_end_decay = episodes//2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
